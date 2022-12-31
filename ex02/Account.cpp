87497b5b68e70466728f8a524b5f17f1841b292a
Yihyun Choi <yichoi@c8r1s8.42seoul.kr>
2022-12-31 23:15:17 +0900

    7

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -5,7 +5,7 @@
 Account::Account(int initial_deposit)
 {
 	this->_accountIndex = _nbAccounts++;
-	this->_amount = initial_deposits;
+	this->_amount = initial_deposit;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
 
@@ -28,27 +28,27 @@ Account::~Account(void)
 		" | Amount: " << checkAmount() << " | closed" << std::endl;
 }
 
-static int	Account::getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
 	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
-	return (_totalDeposits);
+	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
 }
 
-static void	Account::displayAccountsInfos(void)
+void	Account::displayAccountsInfos(void)
 {
 	// 디스플레이 계정 정보 보여주기?
 	_displayTimestamp();
@@ -62,12 +62,37 @@ static void	Account::displayAccountsInfos(void)
 void	Account::makeDeposit(int deposit)
 {
 	// 예금정보 만들기?
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_displayTimestamp();
+	std::cout << \
+		"Accounts Index: " << _accountIndex << \
+		" | p_amount: " << checkAmount() << \
+		" | deposit: " << deposit << \
+		" | amount: " << checkAmount() + deposit << \
+		" | nb_deposits: " << _nbDeposits << std::endl;
+	_amount += deposit;
+	_totalAmount += deposit;
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
 	// 인출 정보 만들기?
-	
+	if (checkAmount() < withdrawal)
+	{
+	}
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_displayTimestamp();
+	std::cout << \
+		"Accouts Index: " << _accountIndex << \
+		" | p_amount: " << checkAmount() << \
+		" | withdrawal: " << withdrawal << \
+		" | amount: " << checkAmount() - withdrawal << \
+		" | nb_withdrawals: " << _nbWithdrawals << std::endl;
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	return (true);
 }
 
 int	Account::checkAmount( void ) const
@@ -85,13 +110,13 @@ void	Account::displayStatus( void ) const
 		" | Withdrawals: " << _nbWithdrawals << std::endl;
 }
 
-static void	Account::_displayTimestamp( void )
+void	Account::_displayTimestamp( void )
 {
 	// 디스플레이 타임 스탬프. 시간?
 	time_t		t;
 	char	buf[16];
 
 	time(&t);
-	strftime(buf, "%Y%m%d_%H%M%S", localtime(&t));
+	strftime(buf, sizeof(buf), "%Y%m%d_%H%M%S", localtime(&t));
 	std::cout << "* " << buf << " *" << std::endl;
 }
