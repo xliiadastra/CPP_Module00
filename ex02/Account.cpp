2cbac3d44614fed266630adced30db717a552f40
Yihyun Choi <yichoi@c8r1s8.42seoul.kr>
2022-12-31 22:44:31 +0900

    6

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,45 +1,62 @@
 #include "Account.hpp"
 #include <ctime>
+#include <iostream>
 
 Account::Account(int initial_deposit)
 {
-	this->_nbAccounts = 0;
-	this->_totalAmount = 0;
-	this->_totalNbDeposits = 0;
-	this->_totalNbWithdrawals = 0;
-	this->_accountIndex = 0;
-	this->_amount = 0;
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = initial_deposits;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
+
+	this->_totalAmount += _amount;
+	_displayTimestamp();
+	std::cout << \
+		"Account Index: " << _accountIndex << \
+		" | amount: " << checkAmount() << " | created" << std::endl;
 }
 
 Account::~Account(void)
 {
-}
-
-Account::Account(void)
-{
+	_nbAccounts--;
+	_totalAmount -= _amount;
+	_totalNbDeposits -= _nbDeposits;
+	_totalNbWithdrawals -= _nbWithdrawals;
+	_displayTimestamp();
+	std::cout << \
+		"Account Index: " << _accountIndex << \
+		" | Amount: " << checkAmount() << " | closed" << std::endl;
 }
 
 static int	Account::getNbAccounts(void)
 {
+	return (_nbAccounts);
 }
 
 static int	Account::getTotalAmount(void)
 {
+	return (_totalAmount);
 }
 
 static int	Account::getNbDeposits(void)
 {
+	return (_totalDeposits);
 }
 
 static int	Account::getNbWithdrawals(void)
 {
+	return (_totalNbWithdrawals);
 }
 
 static void	Account::displayAccountsInfos(void)
 {
 	// 디스플레이 계정 정보 보여주기?
+	_displayTimestamp();
+	std::cout << \
+		"Accounts: " << getNbAccounts() << \
+		" | Total Amount: " << getTotalAmount() << \
+		" | Deposits: " << getNbDeposits() << \
+		" | Withdrawals: " << getNbWithdrawals() << std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
@@ -50,14 +67,22 @@ void	Account::makeDeposit(int deposit)
 bool	Account::makeWithdrawal(int withdrawal)
 {
 	// 인출 정보 만들기?
+	
 }
 
 int	Account::checkAmount( void ) const
 {
+	return (_amount);
 }
 
 void	Account::displayStatus( void ) const
 {
+	_displayTimestamp();
+	std::cout << \
+		"Account index: " << _accountIndex << \
+		" | Amount: " << checkAmount() << \
+		" | Deposits: " << _nbDeposits << \
+		" | Withdrawals: " << _nbWithdrawals << std::endl;
 }
 
 static void	Account::_displayTimestamp( void )
